5dfa46e16dd087e00f8bc26616f82fb4b3897e01
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 19:25:17 +0800

    Added Pseudocode for printing not equal

--- print_not_equal_when_user_inputs_two_different_numbers.py
+++ print_not_equal_when_user_inputs_two_different_numbers.py
@@ -1,1 +1,4 @@
 # B2 Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+# Ask for user input
+# If-else statement to determine if the numbers are not equal
