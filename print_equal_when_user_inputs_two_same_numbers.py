7bd27c3839bc21cce94e7388ed78bb8c951cf385
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 15:23:21 +0800

    Code Written: print equal when two user input are the same

--- print_equal_when_user_inputs_two_same_numbers.py
+++ print_equal_when_user_inputs_two_same_numbers.py
@@ -1,4 +1,11 @@
 # B1 Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 # User Input
+num1 = float(input("First Number: "))
+num2 = float(input("Second Number: "))
+
 # If-else statement for determining equal number inputs
+if num1 == num2:
+    print("Equal")
+else:
+    print("Unequal")
