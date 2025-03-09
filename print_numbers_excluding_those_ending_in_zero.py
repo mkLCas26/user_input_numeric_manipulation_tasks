3af10b7968221839056b5e3d3daef1250cf253fc
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 18:16:37 +0800

    Added Pseudocode for not printing numbers ending in zero

--- print_numbers_excluding_those_ending_in_zero.py
+++ print_numbers_excluding_those_ending_in_zero.py
@@ -1,1 +1,6 @@
 # B1 Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# Initialize empty list
+# For loop to print numbers from 0-100
+# Determine numbers not ending in zero to exclude those that end in zero
+# Print list
