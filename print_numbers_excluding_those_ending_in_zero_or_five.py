198b7a1fbdd54910ec7a6a7f855ea40cbeaaeb65
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:58:32 +0800

    Code Written: print all numbers (0-100) except those ending in zero or five

--- print_numbers_excluding_those_ending_in_zero_or_five.py
+++ print_numbers_excluding_those_ending_in_zero_or_five.py
@@ -1,5 +1,12 @@
 # B2 Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # Initialize empty list
-# While loop to print numbers from 0-100
+no_zero_five = []
+
+# For loop to print numbers from 0-100
+for x in range(0, 100):
+    if x % 10 != 0 and x % 5 !=0:         # Determines number not ending in 0 and 5 and excludes those that are ending in 0 and 5
+        no_zero_five.append(x)
+
 # Print list
+print(no_zero_five)
