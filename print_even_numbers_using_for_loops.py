e0acbc7473a79133204a080592ae0edcfd907855
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 18:09:26 +0800

    Add, Code Written: added pseudocode, added code for printing even numbers (0-100) using for loop

--- print_even_numbers_using_for_loops.py
+++ print_even_numbers_using_for_loops.py
@@ -1,1 +1,5 @@
 # B1 Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+# Simple for loop that prints even numbers
+for x in range(0, 100, 2):
+    print(x)
