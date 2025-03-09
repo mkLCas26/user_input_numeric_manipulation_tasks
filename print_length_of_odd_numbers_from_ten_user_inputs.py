e1a5bd0450c7a9f4e32117c06ce417189a5b5e73
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 17:57:39 +0800

    Fix, Add, Code Written: fixed pseudocode, added comments, and added codes for printing the length of odd numbers

--- print_length_of_odd_numbers_from_ten_user_inputs.py
+++ print_length_of_odd_numbers_from_ten_user_inputs.py
@@ -1,5 +1,16 @@
 # B1 Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
-# Initialize counter and other variables
-# While loop for 10 inputs and determining odd numbers
+# Initialize counter and list
+counter = 0
+odd_numbers = []
+
+# While loop for 10 inputs
+while counter < 10:
+    counter += 1
+    ten_numbers = float(input("Enter 10 numbers: "))
+
+    if ten_numbers % 2 != 0:                               # determines odd numbers
+        odd_numbers.append(ten_numbers)
+
 # Print length of odd numbers
+print(len(odd_numbers))
