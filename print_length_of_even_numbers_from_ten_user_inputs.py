a2a78191a9d8c36f3f3a681464a867724f015ef1
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:43:52 +0800

    Code Written: print the length of even numbers from ten user number inputs

--- print_length_of_even_numbers_from_ten_user_inputs.py
+++ print_length_of_even_numbers_from_ten_user_inputs.py
@@ -1,6 +1,17 @@
 # B2 Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 # Initialize counter and empty list
+counter = 0
+even_numbers = []
+
 # While loop for user input
-# Determining even numbers
-# Print 
+while counter < 10:
+    counter += 1
+    ten_numbers = int(input("Enter 10 numbers: "))
+    
+    # Determining even numbers
+    if ten_numbers % 2 == 0:
+        even_numbers.append(ten_numbers)
+
+# Print length of even numbers
+print(len(even_numbers))
