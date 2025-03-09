fe54eca8bcf328107cbf67f731acf3b2cc600912
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:55:08 +0800

    Added Pseudocode for printing numbers excluding zero or five

--- print_numbers_excluding_those_ending_in_zero_or_five.py
+++ print_numbers_excluding_those_ending_in_zero_or_five.py
@@ -1,1 +1,5 @@
 # B2 Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# Initialize empty list
+# While loop to print numbers from 0-100
+# Print list
