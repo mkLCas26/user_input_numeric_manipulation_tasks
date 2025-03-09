de4bfad7f6e9886e03abc27c1875a807d3342f7c
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 16:00:04 +0800

    Code Written: print the quotient (no decimal number) of two user inputs

--- print_quotient_with_no_decimal_of_two_user_number_inputs.py
+++ print_quotient_with_no_decimal_of_two_user_number_inputs.py
@@ -1,4 +1,9 @@
 # B1 Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
 # User Input
+num1 = float(input("First Number: "))
+num2 = float(input("Second Number: "))
+
 # Print quotient w/o decimals (whole number only)
+quo = num1 / num2
+print(int(quo))
