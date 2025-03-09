d3be56e3d3032084c5078ab60a8295269a49ca3f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 18:35:14 +0800

    Code Written: print the smaller number between two user inputs

--- print_smaller_number_from_two_user_inputs.py
+++ print_smaller_number_from_two_user_inputs.py
@@ -1,4 +1,11 @@
 # B2 Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # Ask user input
+num1 = float(input("First Number: "))
+num2 = float(input("Second  Number: "))
+
 # If-else statement for determining the smaller number
+if num1 < num2: 
+    print(num1)
+else:
+    print(num2)
