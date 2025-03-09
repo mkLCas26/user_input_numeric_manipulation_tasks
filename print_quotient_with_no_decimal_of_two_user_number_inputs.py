63bdc71d7a4302ca91b253896135981bf226424f
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:02:35 +0800

    Added Pseudocode for print_quotient_with_no_decimal_of_two_user_number_inputs.py to print quotient (w/o decimal)

--- print_quotient_with_no_decimal_of_two_user_number_inputs.py
+++ print_quotient_with_no_decimal_of_two_user_number_inputs.py
@@ -1,1 +1,4 @@
 # B2 Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# Ask for user input
+# Print quotient w/o decimals (whole number only)
