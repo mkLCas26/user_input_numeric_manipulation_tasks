b05556bb6967d0663909c7a48020d800c905a207
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:23:39 +0800

    Fix: corrected the previous pseudocode written

--- print_sum_of_nine_numbers_minus_the_first_numbers.py
+++ print_sum_of_nine_numbers_minus_the_first_numbers.py
@@ -1,5 +1,5 @@
 # B2 Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
-# Initialize counter and other variables
+# First user input, initialize counter and other variables
 # While loop for user input and difference
 # Print difference
