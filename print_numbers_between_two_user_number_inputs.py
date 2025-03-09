2e02457766d4dffff50211a316770d526ce09609
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 22:18:56 +0800

    Code Written: print numbers in between two numbers provided by the user

--- print_numbers_between_two_user_number_inputs.py
+++ print_numbers_between_two_user_number_inputs.py
@@ -1,4 +1,9 @@
 # B2 Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # Ask user input 
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+
 # For loop to print numbers between two user inputs
+for x in range(num1 + 1, num2):
+    print(x)
