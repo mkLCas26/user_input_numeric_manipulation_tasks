7f5c8f77014c104372e59b19fba78c5e3ab28e28
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 16:38:44 +0800

    Fix and Code Written: fixed pseudocode and added codes for printing the sum of ten user number inputs

--- print_sum_of_ten_user_number_inputs.py
+++ print_sum_of_ten_user_number_inputs.py
@@ -1,5 +1,15 @@
 # B1 Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
-# Initialize Counter
+# Initialize Counter and other variables
+counter = 0
+sum = 0
+
 # While Loop for 10 user inputs and sum
+while counter < 10:
+    counter += 1
+    ten_numbers = float(input("Enter 10 numbers: ")) 
+
+    sum += ten_numbers
+
 # Print sum
+print(sum)
