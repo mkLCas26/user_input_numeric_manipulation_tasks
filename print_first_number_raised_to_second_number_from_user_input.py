1b3801473d01168228688b1d1f8d4ecec8adb660
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 16:12:32 +0800

    Added Pseudocode for printing first num raised to second num

--- print_first_number_raised_to_second_number_from_user_input.py
+++ print_first_number_raised_to_second_number_from_user_input.py
@@ -1,1 +1,4 @@
 # B1 Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# User Input
+# Print first num raised to second num
