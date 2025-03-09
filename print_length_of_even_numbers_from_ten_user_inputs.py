3d51b77098b342d42b1da6a3a03f50efdf6a82fe
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:35:18 +0800

    Added Pseudocode for printing length of even numbers

--- print_length_of_even_numbers_from_ten_user_inputs.py
+++ print_length_of_even_numbers_from_ten_user_inputs.py
@@ -1,1 +1,6 @@
 # B2 Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# Initialize counter and empty list
+# While loop for user input
+# Determining even numbers
+# Print 
