34a906c4f3b3f490e9d740ec4281f7b2792eed52
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 15:12:55 +0800

    Code Written: print the bigger number between two user inputs

--- print_bigger_number_from_two_user_inputs.py
+++ print_bigger_number_from_two_user_inputs.py
@@ -1,4 +1,11 @@
 # B1 Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # User Input
+num1 = float(input("First Number: "))
+num2 = float(input("Second Number: "))
+
 # If-else statement for choosing bigger number
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
