6c15af84f172d2aaa78fe6fd9753b1751e486ce6
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 21:27:24 +0800

    Code Written: print the difference of the first number input and the remaining nine number inputs by user

--- print_sum_of_nine_numbers_minus_the_first_numbers.py
+++ print_sum_of_nine_numbers_minus_the_first_numbers.py
@@ -1,5 +1,15 @@
 # B2 Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # First user input, initialize counter and other variables
+first_input = float(input("Enter 10 numbers: "))
+counter = 0
+
 # While loop for user input and difference
+while counter < 9:
+    counter += 1
+    other_inputs = float(input("Enter 10 numbers: "))
+
+    first_input -= other_inputs
+
 # Print difference
+print(first_input)
