89a5695d88c712791b5d2502fb286ae5fe537f98
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 22:21:32 +0800

    Fix: changed the numeric type of multiple files necessary (float  ->  int)

--- print_bigger_number_from_two_user_inputs.py
+++ print_bigger_number_from_two_user_inputs.py
@@ -1,8 +1,8 @@
 # B1 Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # User Input
-num1 = float(input("First Number: "))
-num2 = float(input("Second Number: "))
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
 
 # If-else statement for choosing bigger number
 if num1 > num2:
--- print_difference_of_two_user_number_inputs.py
+++ print_difference_of_two_user_number_inputs.py
@@ -1,8 +1,8 @@
 # B2 Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # Ask user input
-num1 = float(input("First Number: "))
-num2 = float(input("Second Number: "))
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
 
 # Printing the difference
 difference = num1 - num2
