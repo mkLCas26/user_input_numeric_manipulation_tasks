5b190cf9c702cc8dd2f64d58fe1501e9eeb2a9bd
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 19:51:58 +0800

    Code Written: print not equal when two user inputs are not the same

--- print_not_equal_when_user_inputs_two_different_numbers.py
+++ print_not_equal_when_user_inputs_two_different_numbers.py
@@ -1,4 +1,11 @@
 # B2 Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # Ask for user input
+num1 = float(input("First Number: "))
+num2 = float(input("Second Number: "))
+
 # If-else statement to determine if the numbers are not equal
+if num1 != num2: 
+    print("Not Equal")
+else:
+    print("Equal")
