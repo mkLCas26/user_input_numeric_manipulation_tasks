d4899b87b72923327c8012243635567cfb660ca2
Kristina Cas <m.kristinacas@gmail.com>
2025-03-09 18:21:58 +0800

    Code Written: print all numbers (0-100) excluding numbers ending in zero

--- print_numbers_excluding_those_ending_in_zero.py
+++ print_numbers_excluding_those_ending_in_zero.py
@@ -1,6 +1,11 @@
 # B1 Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # Initialize empty list
+no_zero = []
+
 # For loop to print numbers from 0-100
-# Determine numbers not ending in zero to exclude those that end in zero
+for x in range(0, 100):
+    if x % 10 != 0:         # Determine numbers not ending in zero to exclude those that end in zero
+        no_zero.append(x)
 # Print list
+print(no_zero)
