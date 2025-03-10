fd08506f9457080a9a5b3553142ce4882220b641
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 01:36:46 +0800

    Try Code: second code trial -> reconstructed if statement and list

--- print_numbers_with_no_duplicate.py
+++ print_numbers_with_no_duplicate.py
@@ -3,18 +3,16 @@
 # Initialize counter and empty list
 counter = 0
 no_duplicate = []
-duplicate = []
 
 # While loop for user input
 while counter < 10:
     counter += 1
     ten_numbers = int(input("Enter 10 numbers: "))
+    no_duplicate.append(ten_numbers)
 
     # Determining if user input is already a part of the list
     if ten_numbers in no_duplicate:
-        duplicate.append(ten_numbers)
-    else:
-        no_duplicate.append(ten_numbers) 
+        no_duplicate.remove(ten_numbers)
 
 # Print numbers w/o duplicate
 print(no_duplicate)
