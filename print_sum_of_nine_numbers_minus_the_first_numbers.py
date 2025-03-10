d8e7a1b8f36e042120f7c6a58d042b1df4b87f78
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 01:36:46 +0800

    Fix: fixed code for print_sum_of_nine_numbers_minus_the_first_numbers.py to have better clarity on the user input message

--- print_sum_of_nine_numbers_minus_the_first_numbers.py
+++ print_sum_of_nine_numbers_minus_the_first_numbers.py
@@ -1,13 +1,13 @@
 # B2 Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # First user input, initialize counter and other variables
-first_input = int(input("Enter 10 numbers: "))
-counter = 0
+first_input = int(input("Enter number 1/10: "))
+counter = 1
 
 # While loop for user input and difference
-while counter < 9:
+while counter < 10:
+    other_inputs = int(input(f"Enter number {counter + 1}/10: "))
     counter += 1
-    other_inputs = int(input("Enter 10 numbers: "))
 
     first_input -= other_inputs
 
