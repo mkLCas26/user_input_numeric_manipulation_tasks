4d9ec7a787de56ef091732be6abd1245b1211383
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 02:02:18 +0800

    Code Written: print numbers that has no duplicates from the 10 user inputs (final; used for loop + if statement outside while loop)

--- print_numbers_with_no_duplicate.py
+++ print_numbers_with_no_duplicate.py
@@ -2,17 +2,19 @@
 
 # Initialize counter and empty list
 counter = 0
+numbers = []
 no_duplicate = []
 
 # While loop for user input
 while counter < 10:
+    user_num = int(input(f"Enter number {counter + 1}/10: "))
+    numbers.append(user_num)
     counter += 1
-    ten_numbers = int(input("Enter 10 numbers: "))
-    no_duplicate.append(ten_numbers)
 
-    # Determining if user input is already a part of the list
-    if ten_numbers in no_duplicate:
-        no_duplicate.remove(ten_numbers)
+# Determining if user input is already a part of the list
+for x in numbers:
+    if numbers.count(x) == 1:
+        no_duplicate.append(x)
 
 # Print numbers w/o duplicate
 print(no_duplicate)
