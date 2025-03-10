f0bd15782d8cd6caec7583a64b2cfc1da5a0a920
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 01:18:29 +0800

    Added pseudocode for printing numbers w/o duplicate

--- print_numbers_with_no_duplicate.py
+++ print_numbers_with_no_duplicate.py
@@ -1,1 +1,6 @@
 # B3 Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# Initialize counter and empty list
+# While loop for user input
+# Determining if user input is already a part of the list
+# Print numbers w/o duplicate
