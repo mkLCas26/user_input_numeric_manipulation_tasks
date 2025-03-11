28aba9e2f9005e288400aec82067c6f167c2ade3
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 00:39:46 +0800

    Fix: edited pseudocode to test if try-except will work

--- ask_user_input_until_certain_conditions.py
+++ ask_user_input_until_certain_conditions.py
@@ -1,6 +1,15 @@
 # B3 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 # Initializing empty lists and needed variables 
+numbers = []
+duplicate = []
+
 # First user input
+user_input = input("Enter a number: ")
+
+# Use try to check if user input is a number
+
 # Loop to ask for user input and determine if its invalid
+
 # Determining whether to display "Unique" and "Duplicate"
+
