5b9afc70e23c67c62ce3e6de53f23975778dc3df
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 00:57:38 +0800

    Fix: separated pseudocode for clearer view code and try-except trial

--- ask_user_input_until_certain_conditions.py
+++ ask_user_input_until_certain_conditions.py
@@ -1,15 +1,23 @@
 # B3 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
-# Initializing empty lists and needed variables 
+"""
+Initializing empty lists and needed variables
+Initial user input
+While loop for user input 
+    Use try to check if use input is a number
+    Determining whether to display "Unique" and "Duplicate"
+
+"""
+
 numbers = []
 duplicate = []
 
-# First user input
-user_input = input("Enter a number: ")
 
-# Use try to check if user input is a number
+try: 
+    user_input = float(input("Enter a number: "))
+    print("tama")
+except ValueError:
+    print(f"Invalid! {user_input} is not a number. ")    
 
-# Loop to ask for user input and determine if its invalid
 
-# Determining whether to display "Unique" and "Duplicate"
 
