8693f189b7696726f09b8def96ab6da2a62ee8e9
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 17:00:34 +0800

    Added Pseudocode for printing duplicates only once

--- print_duplicate_only_once_and_all_numbers.py
+++ print_duplicate_only_once_and_all_numbers.py
@@ -1,1 +1,6 @@
 # B3 Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# Initialize counter and empty list
+# While loop for user input
+# Determining duplicate and printing it once
+# Print final list of numbers
