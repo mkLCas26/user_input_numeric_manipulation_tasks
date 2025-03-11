5256b9279149a282562670745da26c37ac7f28c1
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 01:27:28 +0800

    Added Pseudocode for always printing the lowest number

--- ask_user_input_until_invalid_display_lowest_number.py
+++ ask_user_input_until_invalid_display_lowest_number.py
@@ -1,1 +1,9 @@
 # B3 Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+"""
+Initialize empty list and needed variable
+While loop for user input
+    Try-except for invalid inputs
+    Determining the lowest number in the list
+    Displaying the lowest number
+"""
