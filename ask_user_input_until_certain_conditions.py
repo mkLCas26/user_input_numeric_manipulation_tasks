4ccf7a00fe5474391947dbff40351ef5070b730c
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 00:25:08 +0800

    Added Pseudocode for asking user input until certain conditions

--- ask_user_input_until_certain_conditions.py
+++ ask_user_input_until_certain_conditions.py
@@ -1,1 +1,6 @@
 # B3 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# Initializing empty lists and needed variables 
+# First user input
+# Loop to ask for user input and determine if its invalid
+# Determining whether to display "Unique" and "Duplicate"
