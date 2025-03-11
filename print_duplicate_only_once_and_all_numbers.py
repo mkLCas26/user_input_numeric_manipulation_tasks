4de71809df7120296970f3e3097dafe38e827fd5
Kristina Cas <m.kristinacas@gmail.com>
2025-03-11 18:38:59 +0800

    Try Code: first trial -> used while loop and if statement incorrectly resulting to printing an empty list

--- print_duplicate_only_once_and_all_numbers.py
+++ print_duplicate_only_once_and_all_numbers.py
@@ -1,6 +1,17 @@
 # B3 Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 # Initialize counter and empty list
+counter = 0
+duplicate_once = []
+
 # While loop for user input
-# Determining duplicate and printing it once
+while counter < 10:
+    user_input = int(input(f"Enter number {counter + 1}/10: "))
+    counter += 1
+
+    # Determining duplicate and printing it once
+    if user_input in duplicate_once:
+        duplicate_once.remove(user_input)
+
 # Print final list of numbers
+print(duplicate_once)
