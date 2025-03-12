2443384ad6c2e419cc1e89d0600742b04ed1ae6b
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 19:58:46 +0800

    Added Pseudocode for printing the numbers with most duplicates

--- most_duplicate_number.py
+++ most_duplicate_number.py
@@ -1,1 +1,9 @@
 # B4 Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+ 
+"""
+Initialize empty lists or needed variables
+while loop for user input
+    Try-except for determining invalid inputs
+    Determining most duplicates from user input
+    Print number with most duplicate
+"""
