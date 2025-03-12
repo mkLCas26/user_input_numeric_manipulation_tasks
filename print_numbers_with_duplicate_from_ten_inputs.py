1e2ee8a1944ed3dcc3f1d3a687fb5df25b54c90a
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 14:10:00 +0800

    Added Pseudocode for printing numbers with duplicate from ten user inputs

--- print_numbers_with_duplicate_from_ten_inputs.py
+++ print_numbers_with_duplicate_from_ten_inputs.py
@@ -1,2 +1,9 @@
 # B4 Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+"""
+Initialize empty lists and needed variables
+While loop for 10 user inputs 
+    Append input in list
+For loop for determining if number is a duplicate
+Printing numbers with duplicate
+"""
