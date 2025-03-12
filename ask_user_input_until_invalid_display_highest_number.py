59ea09aaa023bc3713bca3a635488be64b98e944
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 22:48:09 +0800

    Added Pseudocode for printing the highest number

--- ask_user_input_until_invalid_display_highest_number.py
+++ ask_user_input_until_invalid_display_highest_number.py
@@ -1,1 +1,9 @@
 # B4 Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+"""
+Initialize empty list or needed variables
+While loop for user input
+    Try-except to determine invalid inputs
+    Determine highest number using if-else
+    Print the highest number
+"""
