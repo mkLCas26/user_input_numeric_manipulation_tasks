170421b47cf913b824adf5a6ab50c02d26dc4312
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 23:39:38 +0800

    Added Pseudocode for printing the average of the user inputs

--- dispay_average_of_user_inputs.py
+++ dispay_average_of_user_inputs.py
@@ -1,1 +1,10 @@
 #  B4 Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+"""
+Initialize empty list or needed variables
+While loop for user input
+    Try-except for determining invalid inputs
+    Append user inputs in list
+    Getting the average of the list
+    Print the average
+"""
