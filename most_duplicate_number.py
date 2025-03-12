a823db1f6fdd0c59a0e09f7fb517f343f3c47a1d
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 20:41:18 +0800

    Try Code: first trial -> displaying number with most duplicates is not properly working

--- most_duplicate_number.py
+++ most_duplicate_number.py
@@ -10,11 +10,32 @@ while loop for user input
 
 numbers = []
 num = 0
+num_count = 0
+most_dupli = 0
+counter = 0
 
 while True:
     user_input = input("\nEnter a number: ")
     try: 
         num = float(user_input)
+        numbers.append(num)
+
+        for x in numbers:
+            num_count = numbers.count(x)
+            if num_count > most_dupli:
+                most_dupli = x
+                print(f"{most_dupli} has the most duplicates.")
+
     except ValueError:
         print(f"Invalid! {user_input} may not be a number or it is a number list.")
         break
+
+# for x in numbers:
+#     num_count = numbers.count(x)
+#     if num_count > most_dupli:
+#         most_dupli = x
+
+
+    
+
+
