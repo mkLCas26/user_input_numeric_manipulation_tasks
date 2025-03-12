a698506006c3f33426eea22b4f3c4016ef105e2c
Kristina Cas <m.kristinacas@gmail.com>
2025-03-12 23:13:02 +0800

    Added Pseudocode for printing number inputs in descending order

--- ask_user_input_until_invalid_display_highest_to_lowest.py
+++ ask_user_input_until_invalid_display_highest_to_lowest.py
@@ -1,1 +1,10 @@
 # B4 Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+"""
+Initialize empty lists or needed variables
+While loop for user input
+    Try-except for determining invalid inputs
+    Storing number inputs in a list
+    Use sort() function for the descending order
+    Print list of number inputs in descending order
+"""
